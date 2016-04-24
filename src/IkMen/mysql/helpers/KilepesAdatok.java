12e5974fd27c84e1820eb34449c670a29475e44b
Sch-Tomi <sch.tom93@gmail.com>
2016-04-24 19:00:29 +0200

    Add GNU GPL v3 Licence to all files

--- src/IkMen/mysql/helpers/KilepesAdatok.java
+++ src/IkMen/mysql/helpers/KilepesAdatok.java
@@ -4,7 +4,36 @@ package IkMen.mysql.helpers;
  *
  * Az ügyfelek adatai és a kilépésénél felmerülő, kilepesiidővel és fiezetendő össszeggel kibővítve.
  *
- * Created by tom on 2016.03.11..
+ * Internet Kávézó Menedzser - Internet kávézó gépbérlés nyílván tartása
+ * Copyright (C) 2016  Schronk Tamás
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Ez a program szabad szoftver; terjeszthető illetve módosítható a
+ * Free Software Foundation által kiadott GNU General Public License
+ * dokumentumában leírtak; akár a licenc 3-as, akár (tetszőleges) későbbi
+ * változata szerint.
+ *
+ * Ez a program abban a reményben kerül közreadásra, hogy hasznos lesz,
+ * de minden egyéb GARANCIA NÉLKÜL, az ELADHATÓSÁGRA vagy VALAMELY CÉLRA
+ * VALÓ ALKALMAZHATÓSÁGRA való származtatott garanciát is beleértve.
+ * További részleteket a GNU General Public License tartalmaz.
+ *
+ * A felhasználónak a programmal együtt meg kell kapnia a GNU General
+ * Public License egy példányát; ha mégsem kapta meg, akkor
+ * tekintse meg a <http://www.gnu.org/licenses/> oldalon.
  */
 public class KilepesAdatok extends UgyfelArray {
 
